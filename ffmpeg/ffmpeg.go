b1352308a6395d97f2107d776566410cc392e35e
Daniel Chote <daniel@chote.com>
2019-02-12 20:09:18 -0500

    testing raw mjpeg passing...

--- ffmpeg/ffmpeg.go
+++ ffmpeg/ffmpeg.go
@@ -21,8 +21,9 @@ var (
 	CaptureHeight int
 	CaptureFPS    int
 
-	InputPixelFormat  string
 	OutputPixelFormat int32
+
+	shouldRecode bool
 
 	Stream *mjpeg.Stream
 )
@@ -43,26 +44,38 @@ func StartCamera() {
 
 	if runtime.GOOS == "darwin" {
 		inputCtx.SetInputFormat("avfoundation")
-		DeviceName = "default"
-		CaptureFPS = 30
-		InputPixelFormat = "uyvy422"
+
+		err := inputCtx.OpenInputWithOptions("default", []gmf.Pair{
+			{Key: "pixel_format", Val: "uyvy422"},
+			{Key: "input_format", Val: "mjpeg"},
+			{Key: "video_size", Val: fmt.Sprintf("%dx%d", CaptureWidth, CaptureHeight)},
+			{Key: "framerate", Val: strconv.Itoa(30)},
+		})
+
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		shouldRecode = true
 	} else {
 		inputCtx.SetInputFormat("video4linux2")
-		DeviceName = "/dev/video0"
-		CaptureFPS = 5
-		InputPixelFormat = "yuv420p"
+
+		err := inputCtx.OpenInputWithOptions("/dev/video0", []gmf.Pair{
+			{Key: "input_format", Val: "mjpeg"},
+			{Key: "video_size", Val: fmt.Sprintf("%dx%d", CaptureWidth, CaptureHeight)},
+			{Key: "framerate", Val: strconv.Itoa(5)},
+		})
+
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		shouldRecode = false
 	}
 
-	err := inputCtx.OpenInputWithOptions(DeviceName, []gmf.Pair{
-		{Key: "pixel_format", Val: InputPixelFormat},
-		{Key: "video_size", Val: fmt.Sprintf("%dx%d", CaptureWidth, CaptureHeight)},
-		{Key: "framerate", Val: strconv.Itoa(CaptureFPS)},
-	})
-
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+	// note shouldRecode should be determined by actual capabilities
 
 	srcVideoStream, err := inputCtx.GetBestStream(gmf.AVMEDIA_TYPE_VIDEO)
 	if err != nil {
@@ -70,81 +83,131 @@ func StartCamera() {
 		return
 	}
 
-	codec, err := gmf.FindEncoder("mjpeg")
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+	inputCtx.Dump()
 
-	cc := gmf.NewCodecCtx(codec)
-	defer gmf.Release(cc)
-
-	cc.SetPixFmt(OutputPixelFormat)
-	cc.SetWidth(CaptureWidth)
-	cc.SetHeight(CaptureHeight)
-	cc.SetTimeBase(gmf.AVR{1, 1})
-
-	if codec.IsExperimental() {
-		cc.SetStrictCompliance(gmf.FF_COMPLIANCE_EXPERIMENTAL)
-	}
-
-	if err := cc.Open(nil); err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	swsCtx := gmf.NewSwsCtx(srcVideoStream.CodecCtx(), cc, gmf.SWS_FAST_BILINEAR)
-	defer gmf.Release(swsCtx)
-
-	dstFrame := gmf.NewFrame().
-		SetWidth(CaptureWidth).
-		SetHeight(CaptureHeight).
-		SetFormat(OutputPixelFormat)
-	defer gmf.Release(dstFrame)
-
-	if err := dstFrame.ImgAlloc(); err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	for packet := range inputCtx.GetNewPackets() {
-		if Stream == nil {
-			break
-		}
-
-		if packet.StreamIndex() != srcVideoStream.Index() {
-			// skip non video streams
-			continue
-		}
-
-		ist, err := inputCtx.GetStream(packet.StreamIndex())
-
-	decode:
-
-		frame, err := packet.Frames(ist.CodecCtx())
+	if shouldRecode {
+		codec, err := gmf.FindEncoder("mjpeg")
 		if err != nil {
-			// Retry if EAGAIN
-			if err.Error() == "Resource temporarily unavailable" {
-				goto decode
-			}
-			log.Fatal(err)
-		}
-
-		swsCtx.Scale(frame, dstFrame) // TODO I really want to get rid of this!
-
-		p, err := dstFrame.Encode(cc)
-		if err != nil {
-			gmf.Release(p)
 			log.Fatal(err)
 			return
 		}
 
-		Stream.Update(p.Data())
+		cc := gmf.NewCodecCtx(codec)
+		defer gmf.Release(cc)
 
-		gmf.Release(p)
-		gmf.Release(frame)
-		gmf.Release(packet)
+		cc.SetPixFmt(OutputPixelFormat)
+		cc.SetWidth(CaptureWidth)
+		cc.SetHeight(CaptureHeight)
+		cc.SetTimeBase(gmf.AVR{1, 1})
+
+		if codec.IsExperimental() {
+			cc.SetStrictCompliance(gmf.FF_COMPLIANCE_EXPERIMENTAL)
+		}
+
+		if err := cc.Open(nil); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		swsCtx := gmf.NewSwsCtx(srcVideoStream.CodecCtx(), cc, gmf.SWS_FAST_BILINEAR)
+		defer gmf.Release(swsCtx)
+
+		dstFrame := gmf.NewFrame().
+			SetWidth(CaptureWidth).
+			SetHeight(CaptureHeight).
+			SetFormat(OutputPixelFormat)
+		defer gmf.Release(dstFrame)
+
+		if err := dstFrame.ImgAlloc(); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		for packet := range inputCtx.GetNewPackets() {
+			if Stream == nil {
+				break
+			}
+
+			if packet.StreamIndex() != srcVideoStream.Index() {
+				// skip non video streams
+				continue
+			}
+
+			ist, err := inputCtx.GetStream(packet.StreamIndex())
+
+		decode:
+
+			frame, err := packet.Frames(ist.CodecCtx())
+			if err != nil {
+				// Retry if EAGAIN
+				if err.Error() == "Resource temporarily unavailable" {
+					goto decode
+				}
+				log.Fatal(err)
+			}
+
+			swsCtx.Scale(frame, dstFrame) // TODO I really want to get rid of this!
+
+			p, err := dstFrame.Encode(cc)
+			if err != nil {
+				gmf.Release(p)
+				log.Fatal(err)
+				return
+			}
+
+			Stream.Update(p.Data())
+
+			gmf.Release(p)
+			gmf.Release(frame)
+			gmf.Release(packet)
+		}
+
+	} else {
+		for packet := range inputCtx.GetNewPackets() {
+			if Stream == nil {
+				break
+			}
+
+			if packet.StreamIndex() != srcVideoStream.Index() {
+				// skip non video streams
+				continue
+			}
+			/*
+					ist, err := inputCtx.GetStream(packet.StreamIndex())
+
+				decode:
+
+					frame, err := packet.Frames(ist.CodecCtx())
+					if err != nil {
+						// Retry if EAGAIN
+						if err.Error() == "Resource temporarily unavailable" {
+							goto decode
+						}
+						log.Fatal(err)
+					}
+
+						swsCtx.Scale(frame, dstFrame) // TODO I really want to get rid of this!
+
+						p, err := dstFrame.Encode(cc)
+						if err != nil {
+							gmf.Release(p)
+							log.Fatal(err)
+							return
+						}
+
+						Stream.Update(p.Data())
+			*/
+
+			//packet.Dump()
+			Stream.Update(packet.Data())
+
+			//gmf.Release(p)
+			//gmf.Release(frame)
+			gmf.Release(packet)
+		}
+
 	}
+
 }
 
 func StopCamera() {
